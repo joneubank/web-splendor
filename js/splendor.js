d63f91a2be19716b5520684715beedae25c7fb32
= <joneubank@gmail.com>
2015-12-31 03:43:31 -0500

    shuffle method added, ids for market level groups

--- js/splendor.js
+++ js/splendor.js
@@ -1,11 +1,44 @@
+
 var Game = (function()
 {
     'use strict';
     var game = {};
+/*
+Fisher-Yates shuffle
+Code from Mike Bostock @ http://bost.ocks.org/mike/shuffle/
+*/
+    game.shuffle = function(array) {
+        var m = array.length, t, i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+
+        return array;
+    };
+
+    var _white = 0;
+    var _blue  = 1;
+    var _green = 2;
+    var _red   = 3;
+    var _black = 4;
 
     game.color = function(index)
     {
-        var options = ['white', 'blue', 'green', 'red', 'black'];
+        var options = [];
+        options[_white] = 'white';
+        options[_blue]  = 'blue';
+        options[_green] = 'green';
+        options[_red]   = 'red';
+        options[_black] = 'black';
         return options[index];
     };
 
@@ -86,6 +119,48 @@ var Game = (function()
 
         };
     };
+
+    game.decks = {};
+    game.decks.level1 = [
+    /*
+    white       1u+1g+1r+1k         1   1   1   1
+                1u+2g+1r+1k         1   2   1   1
+                2u+2g+1k            2   2       1
+                3w+1u+1k        3   1           1
+                2r+1k                       2   1
+                2u+2k               2           2
+                3u                  3
+            1   4g                      4
+    */
+        game.Development(_white,0,[0,1,1,1,1]),
+        game.Development(_white,0,[0,1,2,1,1]),
+        game.Development(_white,0,[0,2,2,0,1]),
+        game.Development(_white,0,[3,1,0,0,1]),
+        game.Development(_white,0,[0,0,0,2,1]),
+        game.Development(_white,0,[0,2,0,0,2]),
+        game.Development(_white,0,[0,3,0,0,0]),
+        game.Development(_white,1,[0,0,4,0,0]),
+    /*
+    blue        1w+1g+1r+1k 1       1   1   1
+                1w+1g+2r+1k 1       1   2   1
+                1w+2g+2r    1       2   2
+                1u+3g+1r        1   3   1
+                1w+2k       1               2
+                2g+2k               2       2
+                3k                          3
+            1   4r                      4
+    */
+        game.Development(_blue ,0,[1,0,1,1,1]),
+        game.Development(_blue ,0,[1,0,1,2,1]),
+        game.Development(_blue ,0,[1,0,2,2,0]),
+        game.Development(_blue ,0,[0,1,3,1,0]),
+        game.Development(_blue ,0,[1,0,0,0,2]),
+        game.Development(_blue ,0,[0,0,2,0,2]),
+        game.Development(_blue ,1,[0,0,0,0,3]),
+        game.Development(_blue ,1,[0,0,0,4,0])
+    ];
+
+
 
     return game;
 
